9584bc7ef67bfd8afb677a4ec56c314405ae0256
Albert Yu <dev.yufan@gmail.com>
2012-10-07 23:25:59 +0800

    初始化项目代码管理仓库

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Familyforus::Application.config.secret_token = '8295977a991fff0a6e00bbe35c63623cb94f32f74451dfbcf3af59bff7f20cfa73d2524cd304faccbde79492fad2aaa162e2ac8a7520d6e7de1c897a072250fc'
